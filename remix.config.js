f19dc34d012cacabfe7017fdb2555b86d05acfbb
u404 <rich@nontechieguides.io>
2024-07-19 13:40:02 -0500

    Starting from scratch with a clean slate

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  // other configs...
-  serverDependenciesToBundle: ["@remix-run/react", "@remix-run/node"],
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildPath: "build/index.js",
-  serverModuleFormat: "cjs",
-};
