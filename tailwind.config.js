ee3bc3927bef5bf91feeed2c324e6127a701df66
u404 <rich@nontechieguides.io>
2024-07-19 21:10:51 -0500

    ads static image and text w/o proper positioning

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./lp/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        breamcatcher: ['Breamcatcher', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
