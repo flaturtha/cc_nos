c9135db59b8030e7b4ba1c14de83de68fc5632a4
u404 <rich@nontechieguides.io>
2024-08-10 09:11:51 -0500

    fixes remix vite issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'app'),
-      // Ensure that routes are correctly resolved if needed
-      '~/routes': path.resolve(__dirname, 'app/routes'),
-    },
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 8000,
-    fs: {
-      allow: ['..'], // Check that files are in allowed directories
-    },
-  },
-  build: {
-    target: 'esnext',
-  },
-})
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
