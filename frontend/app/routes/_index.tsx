e3fcb2f7d8e9117a43cb8553dc5759582e8488e6
Rich Cook <flaturtha@users.noreply.github.com>
2025-04-08 18:53:29 +0000

    Update _index.tsx
    sets redirect to shopify store

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -2,7 +2,7 @@ import { redirect } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = async () => {
-  return redirect("/lp/coming-soon2?utm_source=1");
+  return redirect("store.talesofmurder.com");
 };
 
 export default function Index() {
